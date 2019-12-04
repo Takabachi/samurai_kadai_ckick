f4586434cbfc13b9e60fb93a7ed6b02d297481e2
Takabachi <hujiisistem@gmail.com>
2019-12-04 12:02:51 +0900

    ・クリック時に画像がズレる挙動を修正 ・clearRectで一回クリックする事に描画を消す

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 canvas = document.getElementById ('sample');
 ctx = canvas.getContext ('2d');   
 
-function draw() {
-	img = new Image();
-    img.src = "shougi.png";
-	ctx.drawImage (img , x , y);
-}
+img = new Image();
+img.src = "shougi.png";
 
 let x = 0;
 let y = 0;
 
 function onClick(e) {
+
+    // 一度描画をクリア
+    ctx.clearRect(0, 0, 600, 600);
     
+    //描画位置(整数にて計算)
     let rect = e.target.getBoundingClientRect();
-	x = e.clientX - rect.left;
-    y = e.clientY - rect.top;
+	x = e.clientX - Math.floor(rect.left);
+    y = e.clientY - Math.floor(rect.top);
     
-    draw();
+    //画像の表示位置を0ではなく画像の縦横半分の大きさを指定してクリックしたとき中央にする
+    ctx.drawImage (img , x-64 , y-64);
 
 }
 
