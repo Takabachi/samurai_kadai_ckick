6457df11e298e8ce0f9b3cbaaeb30cd03ff27008
Takabachi <hujiisistem@gmail.com>
2019-12-04 21:17:25 +0900

    プルリクエスト後の１回目の修正コミット

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-canvas = document.getElementById ('sample');
-ctx = canvas.getContext ('2d');   
+var canvas = document.getElementById ('sample');
+var ctx = canvas.getContext ('2d');   
 
-img = new Image();
+var img = new Image();
 img.src = "shougi.png";
 
-let x = 0;
-let y = 0;
+var x = 0;
+var y = 0;
+var w = canvas.clientWidth;
+var h = canvas.clientHeight;
+
 
 function onClick(e) {
 
     // 一度描画をクリア
-    ctx.clearRect(0, 0, 600, 600);
+    ctx.clearRect(0, 0, w, h);
     
     //描画位置(整数にて計算)
-    let rect = e.target.getBoundingClientRect();
-	x = e.clientX - Math.floor(rect.left);
+    var rect = e.target.getBoundingClientRect();
+    x = e.clientX - Math.floor(rect.left);
     y = e.clientY - Math.floor(rect.top);
     
     //画像の表示位置を0ではなく画像の縦横半分の大きさを指定してクリックしたとき中央にする
-    ctx.drawImage (img , x-64 , y-64);
+    ctx.drawImage (img , x-img.width/2 , y-img.height/2);
 
 }
 
