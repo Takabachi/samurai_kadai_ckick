3763c2c157e533f495413ea958bbebe73cf9a252
Takabachi <hujiisistem@gmail.com>
2019-12-05 19:04:59 +0900

    Remove - 関数名とカッコの間にあるスペースを削除

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 var canvas = document.getElementById('sample');
 var ctx = canvas.getContext('2d');   
 
-var img = new Image();
-img.src = "shougi.png";
+function draw() {
+	img = new Image();
+    img.src = "shougi.png";
+	ctx.drawImage (img , x , y);
+}
 
-var x = 0;
-var y = 0;
-var w = canvas.clientWidth;
-var h = canvas.clientHeight;
-
+let x = 0;
+let y = 0;
 
 function onClick(e) {
-
-    // 一度描画をクリア
-    ctx.clearRect(0, 0, w, h);
     
-    //描画位置(整数にて計算)
-    var rect = e.target.getBoundingClientRect();
-    x = e.clientX - Math.floor(rect.left);
-    y = e.clientY - Math.floor(rect.top);
+    let rect = e.target.getBoundingClientRect();
+	x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
     
     //画像の表示位置を0ではなく画像の縦横半分の大きさを指定してクリックしたとき中央にする
     ctx.drawImage(img , x-img.width/2 , y-img.height/2);
